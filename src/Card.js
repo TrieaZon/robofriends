be51dcb207a7f067182c4d51cc84e550b1f34562
trieazon <trieason@gmail.com>
2023-11-27 12:05:23 +0000

    more styling and code cleanup

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
     return (
-        <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='bg-washed-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${ id }`} />
             <>
                 <h2>{ name }</h2>
