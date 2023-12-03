336fe72d6b613f977ecd7012fcad362d38f01803
trieazon <trieason@gmail.com>
2023-12-03 10:21:39 +0000

    organized into folders, setup api call for robots info, finished styling.

---
+++ src/components/Scroll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        <div style={{ overflowY: 'scroll', borderTop: '2px solid grey',
+            height: '75vh'
+        }}>
+            {props.children}
+        </div>
+    );
+};
+
+export default Scroll;
